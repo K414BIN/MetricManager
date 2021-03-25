878fbb204c658f2ccaeb4abfb6adadab77f3f9d3
Alexander Kalabin <k414bin@yandex.ru>
2021-03-25 19:12:48 +0300

    Initial commit

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace MetricManagerTests
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ValuesHolder>();
+            //services.AddSingleton<ValuesHolder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
